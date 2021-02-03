0df60a4ffa17fcd88f510b1d5ce5a831c27673de
anggiie <anggiie02>
2021-02-03 16:31:44 +0100

    add script

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-   var count = new Date("may 30,2020 00:00:00").getTime();
+   var count = new Date("dec 31,2021 00:00:00").getTime();
     var x = setInterval(function() {
         var now = new Date().getTime();
         var d = count - now;
